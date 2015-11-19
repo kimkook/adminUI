6ed9bc645001eda60138791b7a3da008d6676103
hyungkook <sanaloveyou81@gmail.com>
2015-11-19 14:45:14 +0900

    bower

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-angular.module('app.admin',['ui.router','ui.bootstrap'])
+angular.module('app.admin',['ui.router','ui.bootstrap','ui.grid', 'ui.grid.pagination'])
     .controller('MainController', MainController);
 
 function MainController($http, $scope, $modal, $state) {
